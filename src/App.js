b6bd50c7ebea1d1654aae3bcf9fe44c7951415c7
Ashrf Ali <ashrftvm1@gmail.com>
2019-09-30 16:34:32 +0400

    simple function invoke

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const sayHello = () => {
+    console.log("Hello you invoked me!!");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={sayHello}>Submit</button>
       </header>
     </div>
   );
