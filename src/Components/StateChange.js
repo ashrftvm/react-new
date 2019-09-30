06ee087833f062469766ecd611bc2478dd1bfa48
Ashrf Ali <ashrftvm1@gmail.com>
2019-09-30 17:11:35 +0400

    use state with destructuring

--- src/Components/StateChange.js
+++ src/Components/StateChange.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 
 function StateChange() {
+  const [isRed, setRed] = useState(false);
   const [count, setCount] = useState(0);
+
   const increment = () => {
     setCount(count + 1);
+    setRed(!isRed);
   };
   return (
     <div>
+      <h1 className={isRed ? "red" : ""}>Change my color to Red</h1>
       <button onClick={increment}>Increment</button>
       <p>{count}</p>
     </div>
