f34ec8a51629d75f7961f913e52dbee58d200740
Ashrf Ali <ashrftvm1@gmail.com>
2019-09-30 16:39:03 +0400

    component nesting

--- src/App.js
+++ src/App.js
@@ -2,16 +2,14 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  const sayHello = () => {
-    console.log("Hello you invoked me!!");
-  };
+import SayHello from "./Components/SayHello";
 
+function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={sayHello}>Submit</button>
+        <SayHello />
       </header>
     </div>
   );
---
+++ src/Components/SayHello.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SayHello() {
+  const sayHello = () => {
+    console.log("Hey You invoked me!!");
+  };
+  return (
+    <div>
+      <button onClick={sayHello}>Say Hello</button>
+    </div>
+  );
+}
+
+export default SayHello;
